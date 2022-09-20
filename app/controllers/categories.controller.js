6754f42c0ee3f690bc360e319c574904a8bc8eee
unknown <alinaqvi514@hotmail.com>
2022-09-20 14:39:13 -0700

    modified category update controller

--- app/controllers/categories.controller.js
+++ app/controllers/categories.controller.js
@@ -2,6 +2,7 @@
 const db = require("../models");
 // get categories model from db parameters
 const categories = db.categories;
+const cars = db.cars
 
 
 
@@ -9,47 +10,65 @@ const categories = db.categories;
 exports.getAll = (req, res) => {
     // get all categories from db
     categories.find({})
-    .then(data => {
-        // send all categories to frontend
-        res.status(200).send(data);
-    })
-    .catch(err => {
-        // send error to frontend
-        res.status(500).send({
-            message:
-            err.message || "Some error occurred while retrieving categories."
+        .then(data => {
+            // send all categories to frontend
+            res.status(200).send(data);
+        })
+        .catch(err => {
+            // send error to frontend
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving categories."
+            });
         });
-    });
 };
 
 
 //============================= update category by id ===============================
-exports.update = (req, res) => {
+exports.update = async (req, res) => {
     // get category id from frontend
     const id = req.body.id;
     // get category name from frontend
     const name = req.body.name;
-    
 
-    // update category by id
-    categories.findByIdAndUpdate(id, {name: name})
-    .then(data => {
-       if (!data) {
-            res.status(404).send({
-                message: `Cannot update category with id=${id}. Maybe category was not found!`
+    const previous_name = req.body.previous_name;
+
+
+    //   update all cars with previous category name to new category name 
+    cars.updateMany({ category: previous_name }, { category: name })
+        .then(data => {
+            // update category by id
+            categories.findByIdAndUpdate(id, { name: name })
+                .then(data => {
+                    if (!data) {
+                        res.status(404).send({
+                            message: `Cannot update category with id=${id}. Maybe category was not found!`
+                        });
+                    } else {
+                        // send success message to frontend
+                        res.status(200).send({ message: "Category updated successfully." });
+                    }
+                })
+                .catch(err => {
+                    // send error to frontend
+                    res.status(500).send({
+                        message:
+                            err.message || "Some error occurred while updating the category."
+                    });
+                });
+        })
+        .catch(err => {
+            // send error to frontend
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving cars."
             });
-        } else {
-            // send success message to frontend
-            res.status(200).send({message: "Category updated successfully."});
-        }
-    })
-    .catch(err => {
-        // send error to frontend
-        res.status(500).send({
-            message:
-            err.message || "Some error occurred while updating the category."
         });
-    });
+
+
+
+
+
 };
 
 //================================== delete category by id ===============================
@@ -59,23 +78,23 @@ exports.delete = (req, res) => {
 
     // delete category by id
     categories.findByIdAndRemove(id)
-    .then(data => {
-        if(!data) {
-            res.status(404).send({
-                message: `Cannot delete category with id=${id}. Maybe category was not found!`
+        .then(data => {
+            if (!data) {
+                res.status(404).send({
+                    message: `Cannot delete category with id=${id}. Maybe category was not found!`
+                });
+            } else {
+                // send success message to frontend
+                res.status(200).send({ message: "Category deleted successfully." });
+            }
+        })
+        .catch(err => {
+            // send error to frontend
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while deleting the category."
             });
-        } else {
-            // send success message to frontend
-            res.status(200).send({message: "Category deleted successfully."});
-        }
-    })
-    .catch(err => {
-        // send error to frontend
-        res.status(500).send({
-            message:
-            err.message || "Some error occurred while deleting the category."
         });
-    });
 };
 
 //================================= create category ========================================
@@ -96,7 +115,7 @@ exports.create = (req, res) => {
         .save(category)
         .then(data => {
             // send success message to frontend
-            res.status(200).send({message: "Category created successfully."});
+            res.status(200).send({ message: "Category created successfully." });
         })
         .catch(err => {
             // send error to frontend
